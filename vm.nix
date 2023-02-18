52ab01f9a7726c732ae933b131abf94d4f5970ba
TechsupportOnHold <125736044+TechsupportOnHold@users.noreply.github.com>
2023-02-18 10:52:48 +0000

    Update vm.nix

--- vm.nix
+++ vm.nix
@@ -6,7 +6,7 @@
   programs.dconf.enable = true;
 
   # Add user to libvirtd group
-  users.users.tommy.extraGroups = [ "libvirtd" ];
+  users.users.<YOURUSERNAME>.extraGroups = [ "libvirtd" ];
 
   # Install necessary packages
   environment.systemPackages = with pkgs; [
